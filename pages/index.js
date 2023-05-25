88e153c6a61747aebb69b3abb506de2fdff54b18
Amirhossein Mirsalari <amir.tk7@gmail.com>
2023-05-25 15:02:58 +0330

    get category and main category data from server and pass to navbar component

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,42 @@
+import Head from "next/head";
+
 const Home = () => {
   return (
-    <div>
- دیحی استور
-    </div>
-  )
-}
+    <>
+      <Head>
+        <title>فروشگاه اینترنتی دیجی استور</title>
+        <meta
+          name="description"
+          content="هر آنچه که نیاز دارید با بهترین قیمت از دیجی استور بخرید! جدیدترین انواع گوشی موبایل، لپ تاپ، لباس، لوازم آرایشی و بهداشتی، کتاب، لوازم خانگی، خودرو و... با امکان تعویض و مرجوعی آسان | ✓ارسال رايگان ✓پرداخت در محل ✓ضمانت بازگشت کالا - برای خرید کلیک کنید!
+"
+        />
+      </Head>
+      <Navbar mainCategory={mainCategory} category={category} />
+      <div>دیحی استور</div>
+    </>
+  );
+};
 
 export default Home;
+
+export async function getServerSideProps({ params }) {
+
+  let mainCategoryResponse = await axios.get(
+    "http://localhost:1337/api/main-categories?populate[0]=sliders&populate[1]=banners"
+  );
+  const mainCategory = mainCategoryResponse.data.data.map(
+    (item) => item.attributes
+  );
+
+  let categoryResponse = await axios.get(
+    "http://localhost:1337/api/categories?populate[0]=sub_categories"
+  );
+  const category = categoryResponse.data.data.map((item) => item.attributes);
+
+  return {
+    props: {
+      mainCategory,
+      category,
+    },
+  };
+}
