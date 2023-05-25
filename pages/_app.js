3b42a35fcc113bb5e6bda6ce41742af7998ed687
Amirhossein Mirsalari <amir.tk7@gmail.com>
2023-05-25 15:50:51 +0330

    use redux wrapper

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,20 @@
 import axios from "axios";
 import "@/styles/globals.css";
+import { store } from "redux/store";
+import Head from "next/head";
 
-function MyApp({ Component, pageProps }) {
-  axios.defaults.baseURL = "http://localhost:1337/api";
+axios.defaults.baseURL = "http://localhost:1337/api";
 
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps, router }) {
+  <div className="relative">
+    <Head>
+      <title></title>
+      <meta name="theme-color" content="#673AB6" />
+      <link rel="shortcut icon" href="images/logo.png" />
+    </Head>
+
+    <Component {...pageProps} key={router.asPath} />
+  </div>;
 }
 
-export default MyApp;
+export default store.withRedux(MyApp);
