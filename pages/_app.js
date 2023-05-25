22b5a66a089f17384d33ccf330cbb69202e14dea
Amirhossein Mirsalari <amir.tk7@gmail.com>
2023-05-25 16:15:28 +0330

    fix bug display nothing with add return to the app.js

--- pages/_app.js
+++ pages/_app.js
@@ -6,15 +6,17 @@ import Head from "next/head";
 axios.defaults.baseURL = "http://localhost:1337/api";
 
 function MyApp({ Component, pageProps, router }) {
-  <div className="relative">
-    <Head>
-      <title></title>
-      <meta name="theme-color" content="#673AB6" />
-      <link rel="shortcut icon" href="images/logo.png" />
-    </Head>
+  return (
+    <div className="relative">
+      <Head>
+        <title></title>
+        <meta name="theme-color" content="#673AB6" />
+        <link rel="shortcut icon" href="images/logo.png" />
+      </Head>
 
-    <Component {...pageProps} key={router.asPath} />
-  </div>;
+      <Component {...pageProps} key={router.asPath} />
+    </div>
+  );
 }
 
 export default store.withRedux(MyApp);
