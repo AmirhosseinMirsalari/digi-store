b9eac90e963d9f11af881f68d09c6b8cdec67046
Amirhossein Mirsalari <amir.tk7@gmail.com>
2023-05-25 17:29:50 +0330

    add domain host for iamges

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: [
+      "www.digikala.com",
+      "dkstatics-public.digikala.com",
+      "iili.io",
+      "digikala.arvanvod.com",
+      "encrypted-tbn0.gstatic.com",
+      "digikala.arvanvod.ir",
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
