f65ebe9d6df4985624a085f0427e27aad069540d
Amirhossein Mirsalari <amir.tk7@gmail.com>
2023-05-27 00:13:42 +0330

    fix bug when user enter search page

--- components/SidebarSubCategoryNav.jsx
+++ components/SidebarSubCategoryNav.jsx
@@ -2,11 +2,9 @@ import Link from "next/link";
 import React, { useState } from "react";
 import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
-import { useRouter } from "next/router";
 
 const SidebarSubCategoryNav = ({ category, indexs, mainCategorySlug }) => {
   const [openCategoryNavbar, setOpenCategoryNavbar] = useState(false);
-  const router = useRouter()
 
   const toggleCategoryNavbar = (e) => {
     if (openCategoryNavbar == e) {
@@ -19,7 +17,9 @@ const SidebarSubCategoryNav = ({ category, indexs, mainCategorySlug }) => {
   return (
     <div className="bg-[#f1f2f4] pr-8 border border-x-0 border-t-0 mx-8">
       <li
-        className={`flex items-center justify-between font-bold text-[13px] cursor-pointer text-[#3f4064] h-[52px]  ${openCategoryNavbar ? "text-[#ef4056]" : "text-black"}`}
+        className={`flex items-center justify-between font-bold text-[13px] cursor-pointer text-[#3f4064] h-[52px]  ${
+          openCategoryNavbar ? "text-[#ef4056]" : "text-black"
+        }`}
         key={indexs}
         onClick={() => toggleCategoryNavbar()}
       >
@@ -27,27 +27,22 @@ const SidebarSubCategoryNav = ({ category, indexs, mainCategorySlug }) => {
         <KeyboardArrowDownIcon />
       </li>
 
-      <div
-        className={`  ${
-          openCategoryNavbar ? "block" : "hidden"
-        }`}
-      >
+      <div className={`  ${openCategoryNavbar ? "block" : "hidden"}`}>
         <Link href={`/main/${category.slug}`}>
           <a className="py-4 flex items-center">
             <span className="text-xs text-[#9e9fb1]">همه موارد این دسته</span>
             <KeyboardArrowLeftIcon className="fill-[#9e9fb1] w-4 h-4" />
           </a>
         </Link>
-        {router.pathname === "/" && category.sub_categories.data.map(c=>c.attributes).map((subCategory, inde) => (
-          <div key={inde} className="h-[52px] flex items-center">
-            <p className="text-[#3f4064] text-xs !font-normal">{subCategory.name}</p>
-          </div>
-        ))}
-        {router.pathname === "/search" && category.subCategory.map((subCategory, inde) => (
-          <div key={inde} className="h-[52px] flex items-center">
-            <p className="text-[#3f4064] text-xs !font-normal">{subCategory.name}</p>
-          </div>
-        ))}
+        {category.sub_categories.data
+          .map((c) => c.attributes)
+          .map((subCategory, inde) => (
+            <div key={inde} className="h-[52px] flex items-center">
+              <p className="text-[#3f4064] text-xs !font-normal">
+                {subCategory.name}
+              </p>
+            </div>
+          ))}
       </div>
     </div>
   );
