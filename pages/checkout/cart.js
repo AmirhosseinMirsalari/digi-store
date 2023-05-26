f926676fbaa3e1d87bc1564722288062cd161eb1
Amirhossein Mirsalari <amir.tk7@gmail.com>
2023-05-26 18:44:07 +0330

    get categories and main categories in cart page and pass to navbar component

--- pages/checkout/cart.js
+++ pages/checkout/cart.js
@@ -1,10 +1,36 @@
-const HomePage = () => {
+import Navbar from "components/Navbar";
+import { ResponseToArray } from "lib/ResponseToArray";
+import Head from "next/head";
 
+const HomePage = ({ mainCategories, category }) => {
   return (
-    <div>
-      سبد خرید
-    </div>
-  )
+    <>
+      <Head>
+        <title>سبد خرید</title>
+      </Head>
+      <Navbar mainCategory={mainCategories} category={category} />
+
+    </>
+  );
 };
 
 export default HomePage;
+
+export async function getServerSideProps({ params }) {
+  let mainCategoryResponse = await axios.get(
+    "/main-categories?populate[0]=sliders&populate[1]=banners"
+  );
+  const mainCategories = ResponseToArray(mainCategoryResponse);
+  
+  let categoryResponse = await axios.get(
+    "/categories?populate[0]=sub_categories"
+  );
+  const category = ResponseToArray(categoryResponse);
+
+  return {
+    props: {
+      mainCategories,
+      category,
+    },
+  };
+}
