0ec1093e4b2944dd271259ccbf6cd0d6cea742a2
Amirhossein Mirsalari <amir.tk7@gmail.com>
2023-05-26 14:49:58 +0330

    add digi store services section

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,12 @@ import Navbar from "components/Navbar";
 import HomeSwiper from "components/HomeSwiper"
 import { ResponseToArray } from "lib/ResponseToArray";
 import Head from "next/head";
+import Image from "next/image";
+import { useState } from "react";
 
-const Home = ({ mainCategory, category, homePageDetail }) => {
+const Home = ({ mainCategory, category, homePageDetail,DigistoreSubCategories }) => {
+  const [click, setClick] = useState(false);
+
   return (
     <>
       <Head>
@@ -26,6 +30,45 @@ const Home = ({ mainCategory, category, homePageDetail }) => {
           />
         );
       })}
+
+        {/* Digi store subCategories */}
+        <div className="grid grid-cols-4 grid-rows-2 sm:grid-cols-4 sm:grid-rows-2 lg:grid-cols-8 lg:grid-rows-1 mt-2 mb-1 mx-2 lg:mx-36 ">
+        {DigistoreSubCategories?.slice(0, 7).map((subCategories) => {
+          return (
+            <figure
+              key={subCategories.id}
+              className="flex flex-col items-center cursor-pointer py-2 mx-3"
+            >
+              <Image
+                src={subCategories.image}
+                height={40}
+                width={40}
+                objectFit="cover"
+              />
+              <figcaption className="text-[11px] lg:text-xs text-[#424750] pt-2 font-bold text-center">
+                {subCategories.name}
+              </figcaption>
+            </figure>
+          );
+        })}
+
+        <figure
+          onClick={()=>setClick(true)}
+          className="flex flex-col items-center cursor-pointer py-2 mx-3 rounded-full"
+        >
+          <div className="rounded-full w-10 h-10 border border-black overflow-hidden">
+            <Image
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmbw1OSYrI0szI-VFM283Wa3sxNHFBA41Jig&usqp=CAU"
+              alt=""
+              height={40}
+              width={40}
+              objectFit="cover"
+            />
+          </div>
+          <figcaption className="text-[10px] md:text-xs pt-2">بیشتر</figcaption>
+        </figure>
+      </div>
+      
     </>
   );
 };
@@ -48,11 +91,17 @@ export async function getServerSideProps({ params }) {
   );
   const homePageDetail = ResponseToArray(homePageDetailResponse);
 
+  let DigistoreSubCategoriesResponse = await axios.get(
+    "http://localhost:1337/api/digikala-sub-categoriess"
+  );
+  const DigistoreSubCategories = ResponseToArray(DigistoreSubCategoriesResponse)
+
   return {
     props: {
       mainCategory,
       category,
       homePageDetail,
+      DigistoreSubCategories,
     },
   };
 }
