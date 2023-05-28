73ef68033f02cdbc68943220b530a8f2e4cc934d
Amirhossein Mirsalari <amir.tk7@gmail.com>
2023-05-29 03:13:12 +0330

    create product image section in product header section

--- pages/product/[product].js
+++ pages/product/[product].js
@@ -1,10 +1,22 @@
 import axios from "axios";
 import { ResponseToArray } from "lib/ResponseToArray";
-import React from "react";
+import React, { useState } from "react";
 import { store } from "../../redux/store";
 import Head from "next/head";
 import Navbar from "components/Navbar";
 import Link from "next/link";
+
+// icons
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import NotificationsActiveOutlinedIcon from "@mui/icons-material/NotificationsActiveOutlined";
+import TimelineIcon from "@mui/icons-material/Timeline";
+import BookmarkAddOutlinedIcon from "@mui/icons-material/BookmarkAddOutlined";
+import BookmarkAddIcon from "@mui/icons-material/BookmarkAdd";
+
+// component
+import SingleProductImageMagnify from "../../components/SingleProduct/ImageMagnify";
+import SingleProductThumbnailModalImage from "../../components/SingleProduct/ThumbnailAndModalImage";
+import SingleProductSlider from "../../components/SingleProduct/MobileThumbnailSlider";
 
 const HomePage = ({
   product,
@@ -14,30 +26,99 @@ const HomePage = ({
   category,
   questions,
 }) => {
+
+  const [tabHeart, setTabHeart] = useState(false);
+  const [hoverHeart, setHoverHeart] = useState(false);
+  const [hoverChart, setHoverChart] = useState(false);
+  const [modalMobileSwiper, setModalMobileSwiper] = useState(false);
+  const [productChart, setProductChart] = useState(false);
+
   return (
     <>
       <Head>
         <title>{product.name} | دیجی استور</title>
       </Head>
       <Navbar mainCategory={mainCategory} category={category} />
-         {/* breadcamp */}
-         <div className="py-8 px-4 h-auto w-full items-center hidden lg:flex text-xs text-[#81858b] gap-x-4">
-          <Link href="/">
-            <a>دیجی استور</a>
-          </Link>
-          <div>/</div>
-          <Link href={`/main/${product.mainCategorySlug}`}>
-            <a>{product.mainCategory.replace("-"," ")}</a>
-          </Link>
-          <div>/</div>
-          <Link href={`/search/${product.categorySlug}`}>
-            <a>{product.category.replace("-"," ")}</a>
-          </Link>
-          <div>/</div>
-          <Link href={`/search/${product.subCategorySlug}`}>
-            <a>{product.subCategory.replace("-"," ")}</a>
-          </Link>
+      {/* breadcamp */}
+      <div className="py-8 px-4 h-auto w-full items-center hidden lg:flex text-xs text-[#81858b] gap-x-4">
+        <Link href="/">
+          <a>دیجی استور</a>
+        </Link>
+        <div>/</div>
+        <Link href={`/main/${product.mainCategorySlug}`}>
+          <a>{product.mainCategory.replace("-", " ")}</a>
+        </Link>
+        <div>/</div>
+        <Link href={`/search/${product.categorySlug}`}>
+          <a>{product.category.replace("-", " ")}</a>
+        </Link>
+        <div>/</div>
+        <Link href={`/search/${product.subCategorySlug}`}>
+          <a>{product.subCategory.replace("-", " ")}</a>
+        </Link>
+      </div>
+      {/* product Header*/}
+      <div className="lg:mx-4 lg:grid lg:grid-cols-3 ">
+        {/* product image */}
+        <div className="col-span-1 w-full h-auto bg-white">
+          {/* mobile swiper */}
+          <div className="w-full h-auto !flex flex-col">
+            <div className=" w-full h-auto flex justify-end pt-8 lg:pt-0 ">
+              {/* thumbnail icons */}
+
+              <div className="flex-row-reverse gap-y-3 h-auto flex lg:flex-col items-center  w-auto ">
+                <div
+                  onClick={() => setTabHeart(!tabHeart)}
+                  className="px-3 cursor-pointer "
+                >
+                  <FavoriteIcon
+                    onMouseEnter={() => setHoverHeart(true)}
+                    onMouseLeave={() => setHoverHeart(false)}
+                    className={`${
+                      tabHeart
+                        ? "fill-red-500  "
+                        : "fill-white stroke-black stroke-2"
+                    }`}
+                  />
+                  <div
+                    className={`translate-y-[178px] -translate-x-2 hidden max-w-80 w-fit absolute  bg-[#424750] border border-[#232933] px-2 py-3 top-0 right-12 text-xs rounded z-[999] text-white ${
+                      hoverHeart ? "lg:block" : "lg:hidden"
+                    }`}
+                  >
+                    اضافه به علاقه مندی
+                  </div>
+                </div>
+
+                <div className="px-3 cursor-pointer">
+                  <TimelineIcon
+                    onClick={() => setProductChart(true)}
+                    onMouseEnter={() => setHoverChart(true)}
+                    onMouseLeave={() => setHoverChart(false)}
+                  />
+                  <div
+                    className={`translate-y-[106px] -translate-x-2 hidden  max-w-80 w-fit break-word absolute bg-[#424750] border border-[#232933] px-2 py-3 top-28 z-[999] right-12 text-xs rounded text-white ${
+                      hoverChart ? "lg:block" : "lg:hidden"
+                    }`}
+                  >
+                    نمودار قیمت
+                  </div>
+                </div>
+              </div>
+
+              {/* magnify */}
+
+              <SingleProductImageMagnify product={product} />
+            </div>
+
+            <SingleProductThumbnailModalImage product={product} />
+            <SingleProductSlider
+              modalMobileSwiper={modalMobileSwiper}
+              setModalMobileSwiper={setModalMobileSwiper}
+              product={product}
+            />
+          </div>
         </div>
+      </div>
     </>
   );
 };
