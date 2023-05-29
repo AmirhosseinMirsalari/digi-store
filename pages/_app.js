6cee96c26d839bf016fdcd3c304357406a82a504
Amirhossein Mirsalari <amir.tk7@gmail.com>
2023-05-29 16:30:52 +0330

    add NProgress bar and add scroll to top feature

--- pages/_app.js
+++ pages/_app.js
@@ -3,10 +3,27 @@ import "@/styles/globals.css";
 import { store } from "redux/store";
 import Head from "next/head";
 import Footer from "components/Footer";
+import Router from 'next/router';
+import { useEffect } from "react";
+import NProgress from 'nprogress'
+
 
 axios.defaults.baseURL = "http://localhost:1337/api";
 
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
+
 function MyApp({ Component, pageProps, router }) {
+
+
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }, [router]);
+
   return (
     <div className="relative">
       <Head>
