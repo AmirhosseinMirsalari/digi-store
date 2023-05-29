721d34713484d41b72c2519ecc0004f95613b689
Amirhossein Mirsalari <amir.tk7@gmail.com>
2023-05-29 03:39:09 +0330

    add feature of shop and company

--- pages/product/[product].js
+++ pages/product/[product].js
@@ -519,6 +519,61 @@ const HomePage = ({
           </div>
         </div>
       </div>
+      <div className="hidden lg:flex justify-between py-6 text-xs px-16 text-[#a1a3a8] tex-[11px] border border-b-8 mx-4 my-5">
+          <div className="flex items-center">
+            <div className="ml-1">
+              <Image
+                src="https://www.digikala.com/statics/img/svg/infosection/express-delivery.svg"
+                width={40}
+                height={40}
+              />
+            </div>
+            <p>امکان تحویل اکسپرس</p>
+          </div>
+          <div className="flex items-center">
+            <div className="ml-1">
+              <Image
+                src="https://www.digikala.com/statics/img/svg/infosection/support.svg"
+                width={40}
+                height={40}
+              />
+            </div>
+            <p>24 ساعته، 7 روز هفته</p>
+          </div>
+          <div className="flex items-center">
+            <div className="ml-1">
+              <Image
+                src="https://www.digikala.com/statics/img/svg/infosection/cash-on-delivery.svg"
+                width={40}
+                height={40}
+              />
+            </div>
+
+            <p>امکان پرداخت در محل</p>
+          </div>
+          <div className="flex items-center">
+            <div className="ml-1">
+              <Image
+                src="https://www.digikala.com/statics/img/svg/infosection/days-return.svg"
+                width={40}
+                height={40}
+              />
+            </div>
+
+            <p>هفت روز ضمانت بازگشت کالا</p>
+          </div>
+          <div className="flex items-center">
+            <div className="ml-1">
+              <Image
+                src="https://www.digikala.com/statics/img/svg/infosection/original-products.svg"
+                width={40}
+                height={40}
+              />
+            </div>
+
+            <p>ضمانت اصل بودن کالا</p>
+          </div>
+        </div>
       {/* show toastContainer when add or minus product */}
       <ToastContainer
         position="top-right"
